4266c3471bf91402d255e04abddfff37c1a4c968
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-11 19:45:15 +0400

    Added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
