abe807a3bfc723fe14bc0af6d23fbdd24150aac4
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-17 16:20:46 +0200

    added key attribute

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -28,6 +28,7 @@ const Employees = ({
           <div className="card-collection">
             {employees.map((employee) => (
               <div
+                key={employee.id}
                 id={employee.id}
                 className={
                   employee.teamName === selectedTeam
